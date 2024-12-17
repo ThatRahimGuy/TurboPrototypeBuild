e546f1e2a170c3736b6eadfff127837376edde94
ThatRahimGuy <rahim.ahmed22nd@gmail.com>
2024-12-17 16:31:26 +0000

    up

--- Assets/EndPoint.cs
+++ Assets/EndPoint.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EndPoint : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider collison)
+    {
+        if(collison.tag == "Capsule")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
     void Start()
     {
         
